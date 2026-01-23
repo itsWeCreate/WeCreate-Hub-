dfc9c092056843fde8a86fd20e2d7c1743ea359a
WeCreate <itswecreate@gmail.com>
2026-01-23 11:48:04 -0500

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,4 +1,6 @@
 // IMPORTANT: Replace this placeholder with the actual Web App URL you get after deploying your Google Apps Script.
 // This URL is the bridge between your form and your Google Sheet.
 export const GOOGLE_SHEET_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyq-7Sv8jxsICDRj7B34-DsgZVDrdvBeK45pVLw_YTNOaqqx6xFIQdLuUHRNuNLqFPl/exec';
-export const LEARNING_URL = 'https://ailaunch.netlify.app/';
+export const LEARNING_URL = '#';
+
+// export const LEARNING_URL = 'https://ailaunch.netlify.app/';
